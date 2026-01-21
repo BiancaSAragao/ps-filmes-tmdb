c81b4fca5e702410c1c29b1a9640283f3c2cac52
BiancaSAragão <biancasaragao15@gmail.com>
2026-01-21 17:46:15 -0300

    chore: setup Material UI, axios and project base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from '@/theme/theme';
+import { SnackbarProvider } from 'notistack';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider maxSnack={3}>
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
