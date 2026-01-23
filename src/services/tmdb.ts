f04397df9ec5eac34a57107a5c56891c4f2a3781
BiancaSAragão <biancasaragao15@gmail.com>
2026-01-23 17:28:25 -0300

    chore: clean unused files and api routes

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
+const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+if (!apiKey) {
+  throw new Error('TMDB API key não definida. Verifique o arquivo .env.local');
+}
+
 export const tmdbApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+    api_key: apiKey,
     language: 'pt-BR',
   },
+  timeout: 10000,
 });
